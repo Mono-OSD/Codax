cb6e9b8881db7932829e27d8d7ec98320b89126f
WinterCodes101 <anonymousdabido@gmail.com>
2021-12-07 17:40:37 +1100

    (#1) Created codax.py

---
+++ codax/codax.py
@@ -0,0 +1,22 @@
+"""
+Add the below to your code before you can use these functions:
+- import codax
+- from codax import codax
+
+Then, to use the functions, do something like:
+- codax.setup_wiz()
+"""
+
+def setup_wiz():
+  print("The above is powered by the Codax setup wizard.")
+  other.clearConsole()
+  print("""
+                                                               
+   _|_|_|              _|                                      
+ _|          _|_|    _|_|_|_|              _|    _|  _|_|_|    
+   _|_|    _|_|_|_|    _|      _|_|_|_|_|  _|    _|  _|    _|  
+       _|  _|          _|                  _|    _|  _|    _|  
+ _|_|_|      _|_|_|      _|_|                _|_|_|  _|_|_|    
+                                                     _|        
+                                                     _|     \n\n""")
+  sleep(1.00)
