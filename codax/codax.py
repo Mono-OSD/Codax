da6751ded7db9e84d9abffaffae874e488a46985
MrWinterz98 <anonymousdabido@gmail.com>
2021-12-07 18:36:28 +1100

    (#5) Tweaked and fixed codax.py

--- codax/codax.py
+++ codax/codax.py
@@ -6,10 +6,12 @@ Add the below to your code before you can use these functions:
 Then, to use the functions, do something like:
 - codax.setup_wiz()
 """
+import time
+from time import sleep
+# You can also import your own files here.
 
 def setup_wiz():
-  print("The above is powered by the Codax setup wizard.")
-  other.clearConsole()
+  print("The below is powered by the Codax setup wizard.")
   print("""
                                                                
    _|_|_|              _|                                      
@@ -20,3 +22,10 @@ def setup_wiz():
                                                      _|        
                                                      _|     \n\n""")
   sleep(1.00)
+  name = input("(1) What's your name?\n> ")
+  sleep(1.00)
+  
+  # Feel free to add more code for this function!
+
+# Comment the line below, this is purely for testing
+#setup_wiz()
